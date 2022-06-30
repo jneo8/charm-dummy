90b16f161cf441b5accc3032c900c103aed8a614
jneo8 <james.lin@canonical.com>
2022-06-30 16:47:35 +0800

    feat(apache): Change to install apache with apt

--- src/reactive/dummy.py
+++ src/reactive/dummy.py
@@ -1,10 +1,27 @@
+from charms.reactive import when, when_not, set_flag
+from charmhelpers.core.hookenv import status_set
 import charms.apt
-import charms.reactive as reactive
-import charms_openstack.charm as charm
-import charm.openstack.dummy as dummy
+
+@when_not("apt.installed.apache")
+def install_apache_apt():
+    charms.apt.queue_install(["apache2"])
+    set_flag("apt.installed.apache")
 
 
-@reactive.when_not("charm.installed")
-def install_pkgs():
-    dummy.install()
-    reactive.set_state("charm.installed")
+@when("apt.installed.apache")
+@when_not("apache.ready")
+def install_apache():
+    status_set("blocked", "apache installed, waiting.")
+    # Config here
+    set_flag("apache.ready")
+
+@when_not("apache.ready")
+@when_not("apt.installed.apache")
+def waiting_for_apache():
+    status_set("maintenance", "waiting for apt apache installation")
+
+
+@when("apache.ready")
+def apache_start():
+    status_set("active", "")
+
